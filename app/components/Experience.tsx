e03d7efff58d993cbd714ea854a385a86c37dd6c
onewritescode <lalmohansingh83@gmail.com>
2026-03-23 12:20:37 +0530

    exp merge

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -4,7 +4,7 @@ type ExperinceType = {
   workedSince?: string;
   roleWas: string;
   descriptionOfTheRole: string;
-  jobType: string;
+  jobType?: string;
   startUpLogoImage: string;
   wasRemote: string;
 };
@@ -20,10 +20,8 @@ export default function Experience() {
               startUpLogoImage="/assets/images/thezeroone.png"
               workingAt="The zeroone labs"
               workingSince="dec 2025"
-              workedSince="mar 2026"
               roleWas="full stack developer"
               descriptionOfTheRole="Led development of multiple client websites, building UI components, animations, and backend APIs while managing a small team."
-              jobType="internship"
               wasRemote="remote"
             />
           </div>
@@ -43,16 +41,14 @@ function ExperienceComponent({
   jobType,
   wasRemote,
 }: ExperinceType) {
-    
-    workedSince = workedSince || "current";
 
   return (
     <div className="bg-Dark rounded-2xl max-w-100 bg-black text-white p-2 md:p-4 border border-gray-800">
-        <div className="pl-1 mb-2 sourGummy uppercase flex items-center justify-start gap-1 md:gap-2">
-        <span className="text-xs md:text-2xl font-bold">{roleWas}</span> 
-        <span> • </span>
-        <span className="text-xs text-green-300"> {jobType}</span> 
-        </div>
+      <div className="pl-1 mb-2 sourGummy uppercase flex items-center justify-start gap-1 md:gap-2">
+        <span className="text-xs md:text-2xl font-bold">{roleWas}</span>
+        {jobType && <span> • </span> }
+        <span className="text-xs text-green-300"> {jobType}</span>
+      </div>
 
       <div className="w-full h-10 md:h-12 pb-2 flex items-center justify-between md:gap-10">
         <div className="h-full  gap-2 flex items-center justify-center">
@@ -61,18 +57,29 @@ function ExperienceComponent({
             alt="the zero one llp logo"
             className="h-full rounded-full border border-gray-400"
           />
-          <p className="text-xs md:text-xl sourGummy uppercase text-gray-300 font-semibold">{workingAt}</p>
+          <p className="text-xs md:text-xl sourGummy uppercase text-gray-300 font-semibold">
+            {workingAt}
+          </p>
         </div>
 
-        <p className="text-xs text-gray-200 bg-white/10 border border-gray-800 px-2 py-px rounded-3xl">{wasRemote}</p>
+        <p className="text-xs text-gray-200 bg-white/10 border border-gray-800 px-2 py-px rounded-3xl">
+          {wasRemote}
+        </p>
       </div>
 
       <div>
-        <p className="text-xs md:text-sm text-gray-100 pl-px sourGummy font-medium">{workingSince} -
-             {workedSince ? <span className=""> {workedSince}</span>: "current"}
+        <p className="text-xs md:text-sm text-gray-100 pl-px sourGummy font-medium">
+          {workingSince} -
+          {workedSince ? (
+            <span className=""> {workedSince}</span>
+          ) : (
+            <span className="text-green-400"> current</span>
+          )}
         </p>
 
-        <p className="text-xs md:text-sm pl-px pt-1 text-gray-400 sourGummy line-clamp-3">{descriptionOfTheRole}</p>
+        <p className="text-xs md:text-sm pl-px pt-1 text-gray-400 sourGummy line-clamp-3">
+          {descriptionOfTheRole}
+        </p>
       </div>
     </div>
   );
