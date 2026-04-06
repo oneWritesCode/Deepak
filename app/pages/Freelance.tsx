4cc85c14a5f792698b946c29f8d4406251953da8
onewritescode <lalmohansingh83@gmail.com>
2026-04-06 13:38:10 +0530

    new freelance project

--- app/pages/Freelance.tsx
+++ app/pages/Freelance.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Project from "../components/ProjectComponent";
 // restaurants-web--landing-page.png
 const mashooriya: string = "/assets/video/mashooriya.mp4";
+const xpressprints: string = "/assets/video/xpressprints.mp4";
 const restaurantsWebLandingPage: string =
   "/assets/images/restaurants-web-landing-page.png";
 
@@ -21,11 +22,17 @@ export default function Freelance(): React.ReactElement {
                 title="mashooriya - a music brand"
                 description="website of a music brand to represent them online and show case their music and founders to their fans and followers."
                 category="freelance work"
-              />
+              />  
               <Project
                 image={restaurantsWebLandingPage}
                 title="food restaurant website"
-                description="website for a restaurant to showcase their menu and location."
+                description="website for a restaurant to showcase their work, food quality, menu and location and to improve the digital prescence of the restaurant and to make it easier for customers to find them online."
+                category="freelance work"
+              />
+              <Project
+                video={xpressprints}
+                title="A ecommerce website"
+                description="worked on the maintenance for the website for a ecommerce shop to showcase their products and brand with a proper backend and frontend."
                 category="freelance work"
               />
             </div>
